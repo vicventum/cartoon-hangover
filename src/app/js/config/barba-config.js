e26c25e6e718082f8152002817673e81b75996ad
Víctor Álvarez <victorpool22@gmail.com>
2021-04-17 22:10:25 -0400

    Crea transición con Barba-CSS

--- src/app/js/config/barba-config.js
+++ src/app/js/config/barba-config.js
@@ -1,24 +1,27 @@
-import barba from "../../vendor/barba/barba.min";
+import barba from "../../vendor/barba/barba.min"
+import barbaCss from "../../vendor/barba/barba-css.umd"
+
+barba.use(barbaCss)
 
 barba.init({
-    sync: true,
-    transitions: [
+  preventRunning: true,
+  transitions: [
       {
-        // async once() {
-        //   fadeInContent()
-        // },
-        // async leave() {
-        //   const done = this.async()
-        //   pageTransition()
-        //   fadeOutContent()
-        //   await delay(1200)
-        //   done()
-        // },
-        // async enter() {
-        //   fadeInContent()
-        // },
-  
-  
+        name: 'general',
+        sync: true,
+        once() {},
+        leave() {},
+        enter() {},
+      },
+       {
+        name: 'home',
+        sync: true,
+        to: {
+          namespace: ['home']
+        },
+        once() {},
+        leave() {},
+        enter() {},
       }
     ]
   })
