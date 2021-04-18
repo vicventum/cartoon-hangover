35dbfd5c1c94a2cb937feea4cac59423c8838d76
Víctor Álvarez <victorpool22@gmail.com>
2021-04-18 00:11:07 -0400

    Refactoriza función de tabs compatible con Barba

--- src/app/js/config/barba-config.js
+++ src/app/js/config/barba-config.js
@@ -1,9 +1,6 @@
 import barba from "../../vendor/barba/barba.min"
 import barbaCss from "../../vendor/barba/barba-css.umd"
-import {
-  rootStyles,
-  $id
-} from "../utilities/variables";
+import activeTabs from "../tabs";
 
 barba.use(barbaCss)
 
@@ -11,68 +8,11 @@ barba.init({
   preventRunning: true,
   // Las 'views' son donde se colocará el código que no tiene que ver con las animaciones, pero que se quiere que se ejecute antes o después de las transiciones de cada página en específico.
   views: [
-    // Se ejecutará antes de la animación 'enter' a la página 'architecture'
+    // Corrige error de tabs. Vuelve a ejecutar el código correspondiente a las tabs cada vez que se esta a punto de entrar a la pagina faq.html
     {
       namespace: 'faq',
       beforeEnter() {
-        const tabs = $id('tabs')
-        console.log(tabs)
-        
-        let prevItem = null,
-          currentItem = null
-        
-        if (tabs !== null) {
-          tabs.addEventListener('click', e => {
-            
-            let item = null
-            if (e.target.classList.contains('tabs__item'))
-              item = e.target
-            else if (e.target.parentElement.classList.contains('tabs__item'))
-              item = e.target.parentElement
-        
-            console.log(item.children);
-        
-            // Guardando el item
-            if (item) {
-              currentItem = {
-                item,
-                title: item.children[0],
-                panel: item.children[1]
-              }
-              // Reseteando el item anterior
-              if (prevItem !== null && currentItem.item !== prevItem.item) {
-                manageClass(false)
-                // prevItem.panel.style.maxHeight = 0
-              }
-              // Desplegando el item actual
-              if (currentItem !== null) {
-                const maxHeight = currentItem.panel.scrollHeight
-                rootStyles.setProperty('--height-panel', `calc(${maxHeight}px + 50px)`)
-                toggleClass()
-                // manageClass(true)
-                prevItem = currentItem
-              }
-            }
-          })
-        }
-        
-        function manageClass(mode) {
-          if (mode) {
-            currentItem.item.classList.add('tabs--show')
-            // currentItem.title.classList.add('title-in-hover')
-            // currentItem.panel.classList.add('item-in-hover')
-          } else {
-            prevItem.item.classList.remove('tabs--show')
-            // prevItem.title.classList.remove('title-in-hover')
-            // prevItem.panel.classList.remove('item-in-hover')
-          }
-        }
-        
-        function toggleClass() {
-          currentItem.item.classList.toggle('tabs--show')
-          // currentItem.title.classList.toggle('title-in-hover')
-          // currentItem.panel.classList.toggle('item-in-hover')
-        }
+        activeTabs()
       },
     }
   ],
--- src/app/js/tabs.js
+++ src/app/js/tabs.js
@@ -1,60 +1,65 @@
 import {
   rootStyles,
-  tabs
+  $id
 } from "./utilities/variables";
 
-let prevItem = null,
-  currentItem = null
+export default function activeTabs() {
+  
+  let prevItem = null,
+    currentItem = null
 
-if (tabs !== null) {
-  tabs.addEventListener('click', e => {
-    
-    let item = null
-    if (e.target.classList.contains('tabs__item'))
-      item = e.target
-    else if (e.target.parentElement.classList.contains('tabs__item'))
-      item = e.target.parentElement
-
-    console.log(item.children);
-
-    // Guardando el item
-    if (item) {
-      currentItem = {
-        item,
-        title: item.children[0],
-        panel: item.children[1]
+  const tabs = $id('tabs')
+  
+  if (tabs !== null) {
+    tabs.addEventListener('click', e => {
+      
+      let item = null
+      if (e.target.classList.contains('tabs__item'))
+        item = e.target
+      else if (e.target.parentElement.classList.contains('tabs__item'))
+        item = e.target.parentElement
+  
+      console.log(item.children);
+  
+      // Guardando el item
+      if (item) {
+        currentItem = {
+          item,
+          title: item.children[0],
+          panel: item.children[1]
+        }
+        // Reseteando el item anterior
+        if (prevItem !== null && currentItem.item !== prevItem.item) {
+          manageClass(false)
+          // prevItem.panel.style.maxHeight = 0
+        }
+        // Desplegando el item actual
+        if (currentItem !== null) {
+          const maxHeight = currentItem.panel.scrollHeight
+          rootStyles.setProperty('--height-panel', `calc(${maxHeight}px + 50px)`)
+          toggleClass()
+          // manageClass(true)
+          prevItem = currentItem
+        }
       }
-      // Reseteando el item anterior
-      if (prevItem !== null && currentItem.item !== prevItem.item) {
-        manageClass(false)
-        // prevItem.panel.style.maxHeight = 0
-      }
-      // Desplegando el item actual
-      if (currentItem !== null) {
-        const maxHeight = currentItem.panel.scrollHeight
-        rootStyles.setProperty('--height-panel', `calc(${maxHeight}px + 50px)`)
-        toggleClass()
-        // manageClass(true)
-        prevItem = currentItem
-      }
-    }
-  })
-}
-
-function manageClass(mode) {
-  if (mode) {
-    currentItem.item.classList.add('tabs--show')
-    // currentItem.title.classList.add('title-in-hover')
-    // currentItem.panel.classList.add('item-in-hover')
-  } else {
-    prevItem.item.classList.remove('tabs--show')
-    // prevItem.title.classList.remove('title-in-hover')
-    // prevItem.panel.classList.remove('item-in-hover')
+    })
   }
-}
-
-function toggleClass() {
-  currentItem.item.classList.toggle('tabs--show')
-  // currentItem.title.classList.toggle('title-in-hover')
-  // currentItem.panel.classList.toggle('item-in-hover')
+  
+  function manageClass(mode) {
+    if (mode) {
+      currentItem.item.classList.add('tabs--show')
+      // currentItem.title.classList.add('title-in-hover')
+      // currentItem.panel.classList.add('item-in-hover')
+    } else {
+      prevItem.item.classList.remove('tabs--show')
+      // prevItem.title.classList.remove('title-in-hover')
+      // prevItem.panel.classList.remove('item-in-hover')
+    }
+  }
+  
+  function toggleClass() {
+    currentItem.item.classList.toggle('tabs--show')
+    // currentItem.title.classList.toggle('title-in-hover')
+    // currentItem.panel.classList.toggle('item-in-hover')
+  }
 }
